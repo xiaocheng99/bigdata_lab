6057a4c222b54140dc1539f470ed436f8ccb7e27
changcheng liu <1002620235@qq.com>
2022-10-25 17:52:17 +0800

    添加幻灯片

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,61 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+
+
+<div class="navigate">
+  
+    <div class="logo">
+      <a href>
+        <img >
+      </a>
+    </div>
+    <div class="navMenu">
+        <div class="line"></div>
+        <el-menu 
+            :default-active="activeIndex2"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#2d70c4"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+          
+            <el-menu-item class="navMenu_tittle" index="1">首页</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="2">实验室介绍</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="3">研发平台</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="4">学术成果</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="5">新闻公告</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="6">党团工作</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="7">友情链接</el-menu-item>
+            <el-menu-item class="navMenu_tittle" index="8">联系我们</el-menu-item>
+        </el-menu>
+    </div>
+</div>
+
+  </template>
+
+
+
+
+
+  <script>
+
+    export default {
+      name: 'HomeView',
+      data() {
+        return {
+          activeIndex: '1',
+          activeIndex2: '1'
+        };
+      },
+      methods: {
+        handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      }
+    }
+  
+  </script>
+  
+<style scoped>
+@import url(../assets/css/HomeView.css);
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="homeBody">
+    <div class="header">
+      <el-carousel :interval="5000" arrow="always" class="slider_container" height="100%" >
+        <el-carousel-item v-for="item in 4" :key="item" >
+          <h3>ssss</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+   
   </div>
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+
+
+
+  <script>
+
+    export default {
+      name: 'HomeView',
+      data() {
+        return {
+          activeIndex: '1',
+          activeIndex2: '1'
+        };
+      },
+      methods: {
+        handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      }
+    }
+  
+  </script>
+  
+<style scoped>
+@import url(../assets/css/HomeView.css);
+</style>
